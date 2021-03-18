f694be614ab6fb0ece45ca0c50894f04ddf8baf8
DeCyberPrince <decyberprince@gmail.com>
2021-03-18 20:23:54 +0500

    add actions to form

--- js/index.js
+++ js/index.js
@@ -1,18 +1,97 @@
 const form = document.getElementById('data')
+const commentsWrapper = document.getElementById('comment-wrapper')
+let pickedComment = null
+
+const name = form.querySelector('[name="name"]'),
+    text = form.querySelector('[name="text"]'),
+    email = form.querySelector('[name="email"]')
+
+let commentList = [
+  {id: 1, name: "Вася", email: "vasya@mail.ru", message: "Сообщение от Василия Пупкина."},
+  {id: 2, name: "Маруся", email: "marysia@mail.ru", message: "Всем привет, я Маруся"},
+]
+
+render(commentList)
+
+form.addEventListener('submit', retrieveFormValue)
+
+commentsWrapper.addEventListener('click', function (eventObject){
+  let commentElement = findElementInParents(eventObject.target, 'comment-item')
+  pickComment(+commentElement.dataset.id)
+})
+
+
+/*
+
+
+
+
+
+
+
+Вспомогательные функции
+
+
+
+
+
+
+
+
+ */
+
+function findElementInParents(currentElement, stopClass) {
+  let stopper = 0
+  while (!currentElement.classList.contains(stopClass)) {
+    currentElement = currentElement.parentElement
+    stopper++
+    if (stopper > 10) throw new Error("Слишком глубоко вложен")
+  }
+  return currentElement
+}
+
+function pickComment(commentId) {
+  pickedComment = commentList.find(function (comment){
+    return comment.id === commentId
+  })
+  name.value = pickedComment.name;
+  email.value = pickedComment.email;
+  text.value = pickedComment.message;
+}
+
+function addComment(newComment) {
+  commentList.push(newComment)
+  commentsWrapper.innerHTML = ''
+  render(commentList)
+}
+
+function render(model) {
+  model.forEach(function (comment){
+    commentsWrapper.insertAdjacentHTML('beforeend', getTemplate(comment))
+  })
+}
+
+function getTemplate(comment) {
+  return `
+  <div class="comment-item" data-id="${comment.id}">
+      <h3 class="comment-item__header">${comment.name}</h3>
+      <div class="comment-item__description">
+         <h4 class="email">${comment.email}</h4>
+          <p class="comment-item__message">${comment.message}</p>
+      </div>
+  </div>
+  `
+}
 
 function retrieveFormValue(event) {
   event.preventDefault();
 
-  const name = form.querySelector('[name="name"]'),
-    text = form.querySelector('[name="text"]'),
-    email = form.querySelector('[name="email"]')
-
-  const values = {
+  let newComment = {
+    id: Date.now(),
     name: name.value,
-    text: text.value,
-    email: email.value
+    email: email.value,
+    message: text.value
   }
-  console.log('index', values)
-}
 
-form.addEventListener('submit', retrieveFormValue)
+  addComment(newComment)
+}
